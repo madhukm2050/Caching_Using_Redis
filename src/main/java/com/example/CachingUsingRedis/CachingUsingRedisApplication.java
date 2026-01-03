fabc6d104cb6828e2c3b5bf7e5630af62e4eb0d2
madhu <madhukm2050@gmail.com>
2026-01-03 14:40:59 +0530

    Initial setup

---
+++ src/main/java/com/example/CachingUsingRedis/CachingUsingRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.CachingUsingRedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingUsingRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingUsingRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CachingUsingRedis/CachingUsingRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CachingUsingRedis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingUsingRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
